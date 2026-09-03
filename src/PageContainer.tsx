a2fe9718791adcf4b6ba6697817f989db711cc16
Sabrina Stangler <sabrinastangler51@gmail.com>
2026-09-03 13:41:15 -0500

    add footer, fill in private events info

--- src/PageContainer.tsx
+++ src/PageContainer.tsx
@@ -1,9 +1,16 @@
-import { Link, Outlet } from 'react-router-dom'
-import { FaFacebook, FaInstagram, FaYelp } from 'react-icons/fa'
+import {
+  FaEnvelope,
+  FaFacebook,
+  FaInstagram,
+  FaPhone,
+  FaSearchLocation,
+  FaYelp,
+} from 'react-icons/fa'
+import { Link, NavLink, Outlet } from 'react-router-dom'
 
 export default function PageContainer() {
   return (
-    <div className='flex flex-col items-center min-h-screen pt-5 gap-5'>
+    <div className='flex flex-col items-center min-h-screen pt-5 gap-10'>
       <div>
         <Link to='/'>
           <img
@@ -13,21 +20,58 @@ export default function PageContainer() {
           />
         </Link>
       </div>
+
       <nav className='flex flex-col md:flex-row gap-2 md:gap-5 items-center'>
-        <Link to='/menu'>Menu</Link>
-        <Link to='/beer'>Beer</Link>
-        <Link to='/wine'>Wine</Link>
-        <Link to='/private-events-catering'>Private Parties + Catering</Link>
-        <Link to='/events'>Events</Link>
-        <Link to='/reservations'>Reservations</Link>
-        <Link to='/about'>About</Link>
+        <NavLink
+          to='/menu'
+          className={({ isActive }) => (isActive ? 'text-ub-orange' : '')}
+        >
+          Menu
+        </NavLink>
+        <NavLink
+          to='/beer'
+          className={({ isActive }) => (isActive ? 'text-ub-orange' : '')}
+        >
+          Beer
+        </NavLink>
+        <NavLink
+          to='/wine'
+          className={({ isActive }) => (isActive ? 'text-ub-orange' : '')}
+        >
+          Wine
+        </NavLink>
+        <NavLink
+          to='/private-events-catering'
+          className={({ isActive }) => (isActive ? 'text-ub-orange' : '')}
+        >
+          Private Parties + Catering
+        </NavLink>
+        <NavLink
+          to='/events'
+          className={({ isActive }) => (isActive ? 'text-ub-orange' : '')}
+        >
+          Events
+        </NavLink>
+        <NavLink
+          to='/reservations'
+          className={({ isActive }) => (isActive ? 'text-ub-orange' : '')}
+        >
+          Reservations
+        </NavLink>
+        <NavLink
+          to='/about'
+          className={({ isActive }) => (isActive ? 'text-ub-orange' : '')}
+        >
+          About
+        </NavLink>
       </nav>
-      <div className='flex gap-4 items-center justify-center opacity-70 hover:opacity-100 transition-opacity'>
+
+      <div className='flex gap-4 items-center justify-center transition-opacity'>
         <a
           href='https://facebook.com'
           target='_blank'
           rel='noopener noreferrer'
-          className='text-xl hover:opacity-80 text-orange-700'
+          className='text-xl text-ub-orange hover:text-ub-dark'
         >
           <FaFacebook />
         </a>
@@ -35,7 +79,7 @@ export default function PageContainer() {
           href='https://instagram.com'
           target='_blank'
           rel='noopener noreferrer'
-          className='text-xl hover:opacity-80 text-orange-700'
+          className='text-xl text-ub-orange hover:text-ub-dark'
         >
           <FaInstagram />
         </a>
@@ -43,15 +87,88 @@ export default function PageContainer() {
           href='https://yelp.com'
           target='_blank'
           rel='noopener noreferrer'
-          className='text-xl hover:opacity-80 text-orange-700'
+          className='text-xl text-ub-orange hover:text-ub-dark'
         >
           <FaYelp />
         </a>
       </div>
-      <main style={{ padding: '1rem' }}>
+
+      <main className='flex-1 w-full p-4 text-center'>
         {/* Children routes will render inside the Outlet */}
         <Outlet />
       </main>
+
+      <footer className='bg-ub-dark text-ub-white border-t-6 border-ub-orange w-full p-4 text-center flex gap-4 flex-col md:flex-row justify-around items-center text-sm'>
+        {/* <table className='tight-table max-w-sm text-left border-ub-white border-2'>
+          <tr>
+            <th colSpan={2} className='text-center'>
+              <h2>Hours</h2>
+            </th>
+          </tr>
+          <tr>
+            <td className='font-black pr-1'>Monday, Tuesday</td>
+            <td>
+              Open during Fiserv Arena Events or private party requests
+            </td>
+          </tr>
+          <tr>
+            <td className='font-black pr-1'>Wednesday</td>
+            <td>11 AM - 11 PM</td>
+          </tr>
+          <tr>
+            <td className='font-black pr-1'>Thursday</td>
+            <td>11 AM - 11 PM</td>
+          </tr>
+          <tr>
+            <td className='font-black pr-1'>Friday</td>
+            <td>11 AM - 2:30 AM</td>
+          </tr>
+          <tr>
+            <td className='font-black pr-1'>Saturday</td>
+            <td>11 AM - 2:30 AM</td>
+          </tr>
+          <tr>
+            <td className='font-black pr-1'>Sunday</td>
+            <td>11 AM - 11 PM</td>
+          </tr>
+        </table> */}
+        <div>
+          <h2 className='text-ub-white'>Uncle Buck's on Third</h2>
+          {/* <p>Milwaukee, WI</p> */}
+        </div>
+
+        <div className='flex flex-col md:flex-row items-center gap-4'>
+          <div>
+            {/* <h2>Contact</h2> */}
+            <div className='flex items-center justify-center gap-2 hover:text-ub-dark'>
+              <FaPhone className='text-ub-orange' />
+              (414)-988-0355
+            </div>
+            <div className='flex items-center justify-center gap-2 hover:text-ub-dark'>
+              <FaEnvelope className='text-ub-orange' />
+              connect@unclebucksonthird.com
+            </div>
+          </div>
+
+          <div className='text-center'>
+            {/* <h2>Uncle Bucks</h2> */}
+            <p>1125 N Doctor M.L.K Jr. Drive</p>
+            <p>Milwaukee, WI 53203</p>
+            <div className='text-white flex items-center justify-center gap-2'>
+              {' '}
+              <FaSearchLocation className='text-ub-orange' />
+              <a
+                href='https://www.google.com/maps/dir/?api=1&destination=1125+N+Doctor+M.L.K+Jr.+Drive,+Milwaukee,+WI+53203'
+                target='_blank'
+                rel='noopener noreferrer'
+                className=' hover:text-ub-dark underline'
+              >
+                Get Directions
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,23 @@
-import { FaSearchLocation } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 import HomeVideo from '/videos/home-video.mp4'
+import PrivateSpace1 from '/images/private-space-1.jpeg'
+import PrivateSpace2 from '/images/private-space-2.jpeg'
+import PrivateSpace3 from '/images/private-space-3.jpeg'
+
 export default function Home() {
   return (
-    <div key='home-container' className='flex flex-col items-center gap-5'>
+    <div key='home-container' className='flex flex-col items-center gap-15'>
       <video src={HomeVideo} autoPlay loop muted className='w-full h-auto' />
 
       <h1>Hours</h1>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-2 text-center'>
         <div className='text-left my-4'>
-          <table className=''>
+          <table className='loose-table'>
             <tr>
-              <td className='font-black text-ub-orange pr-1'>Monday, Tuesday</td>
-              <td className='col-span-2'>
-                Open during Fiserv Arena Events or private party requests
+              <td className='font-black text-ub-orange pr-1'>
+                Monday, Tuesday
               </td>
+              <td>Open during Fiserv Arena Events or private party requests</td>
             </tr>
             <tr>
               <td className='font-black text-ub-orange pr-1'>Wednesday</td>
@@ -37,7 +41,7 @@ export default function Home() {
             </tr>
           </table>
         </div>
-        <div className='bg-ub-orange text-ub-white flex flex-col items-center justify-center gap-5 p-5'>
+        <div className='border-2 border-ub-orange flex flex-col items-center justify-center gap-5 p-5'>
           <p>
             Please email us at: connect@unclebucksonthird.com for availability
             during off-business hours
@@ -46,23 +50,28 @@ export default function Home() {
         </div>
       </div>
 
-      <h1>Location</h1>
-      <div className='text-center'>
-        <p className='font-semibold'>Uncle Bucks</p>
-        <p>1125 N Doctor M.L.K Jr. Drive</p>
-        <p>Milwaukee, WI 53203</p>
-        <div className='text-ub-orange flex items-center justify-center gap-2'>
-          {' '}
-          <FaSearchLocation />
-          <a
-            href='https://www.google.com/maps/dir/?api=1&destination=1125+N+Doctor+M.L.K+Jr.+Drive,+Milwaukee,+WI+53203'
-            target='_blank'
-            rel='noopener noreferrer'
-            className=' hover:text-blue-800 underline'
-          >
-            Get Directions
-          </a>
-        </div>
+      <h1>Private Parties, Events + Catering</h1>
+      <button className='bg-ub-orange text-ub-white py-4 px-6 rounded hover:bg-ub-dark'>
+        <Link to='/private-events-catering' className=''>
+          Book Now
+        </Link>
+      </button>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-2 text-center'>
+        <img
+          src={PrivateSpace1}
+          alt='Private Event Space 1'
+          className='w-full h-auto'
+        />
+        <img
+          src={PrivateSpace2}
+          alt='Private Event Space 2'
+          className='w-full h-auto'
+        />
+        <img
+          src={PrivateSpace3}
+          alt='Private Event Space 3'
+          className='w-full h-auto'
+        />
       </div>
     </div>
   )
