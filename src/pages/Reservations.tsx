f9a90778a08753502009a8db06201f4d60119443
Sabrina Stangler <sabrinastangler51@gmail.com>
2026-09-03 22:22:43 -0500

    add toast-linked buttons to reservation page

--- src/pages/Reservations.tsx
+++ src/pages/Reservations.tsx
@@ -1,8 +1,27 @@
 export default function Reservations() {
   return (
-    <div>
+    <div className='flex flex-col items-center gap-8'>
       <h1>Reservations</h1>
       <p>Make a reservation for your next visit!</p>
+      
+      <div className='flex flex-col gap-4'>
+        <a
+          href='https://tables.toasttab.com/restaurants/4b4d1073-0d16-4617-b560-4d1f47f06184/findTime'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='bg-ub-orange text-ub-white py-4 px-6 rounded hover:bg-ub-dark text-center'
+        >
+          Make a Reservation
+        </a>
+        <a
+          href='https://tables.toasttab.com/restaurants/4b4d1073-0d16-4617-b560-4d1f47f06184/joinWaitlist'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='bg-ub-orange text-ub-white py-4 px-6 rounded hover:bg-ub-dark text-center'
+        >
+          Join the Waitlist
+        </a>
+      </div>
     </div>
   )
 }
